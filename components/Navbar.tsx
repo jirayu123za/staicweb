9b493818c3c5b26b0fcac930eee038e9dbefd1e6
jirayu <jirayu004za@gmail.com>
2023-10-08 01:11:18 +0700

    add new nav and footer

---
+++ components/Navbar.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import Image from 'next/image'
+import React from 'react'
+import { NavLinks } from '@/constants'
+
+const Navbar = () => {
+    return (
+        <nav className="flexBetween navBar">
+            <div className="flex-1 flexStart gap-10">
+                <Link href="/">
+                    <Image
+                        src="/logo.svg"
+                        width={115}
+                        height={43}
+                        alt="Flexible"
+                    />
+                </Link>
+
+                <ul className="xl:flex hidden text-small gap-7">
+                    {NavLinks.map((link) => (
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
