250301aa84b314d2849fac7b2bac8581fe37376f
jirayu <jirayu004za@gmail.com>
2023-10-08 21:17:38 +0700

    complete logic

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,11 +2,14 @@ import Link from 'next/link'
 import Image from 'next/image'
 import React from 'react'
 import { NavLinks } from '@/constants'
+import AuthProviders from './AuthProviders'
 
 const Navbar = () => {
+    const session = {};
+
     return (
-        <nav className="flexBetween navBar">
-            <div className="flex-1 flexStart gap-10">
+        <nav className='flexBetween navbar'>
+            <div className='flex-1 flexStart gap-10'>
                 <Link href="/">
                     <Image
                         src="/logo.svg"
@@ -16,13 +19,25 @@ const Navbar = () => {
                     />
                 </Link>
 
-                <ul className="xl:flex hidden text-small gap-7">
+                <ul className='xl:flex hidden text-small gap-7'>
                     {NavLinks.map((link) => (
                         <Link href={link.href} key={link.key}>
                             {link.text}
                         </Link>
                     ))}
                 </ul>
+            </div>
+            <div className='flexCenter gap-4'>
+                {session ? (
+                    <>
+                        UserPhoto
+                        <Link href="/create-project">
+                            share work
+                        </Link>
+                    </>
+                ) : (
+                    <AuthProviders/>
+                    )}
             </div>
         </nav>
     )
