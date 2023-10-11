50307616ab6bb5213cc6296192e66265ff6f783b
jirayu <jirayu004za@gmail.com>
2023-10-11 23:03:51 +0700

    del comment

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,6 +1,5 @@
 import { g, config, auth } from '@grafbase/sdk';
 
-// @ts-ignore
 const User = g.model('User', {
     name: g.string().length({ min: 2, max: 100 }),
     email: g.string().unique(),
@@ -11,7 +10,6 @@ const User = g.model('User', {
     projects: g.relation(() => Project).list().optional(),
 })
 
-// @ts-ignore
 const Project = g.model('Project', {
     title: g.string().length({ min: 3 }),
     description: g.string(), 
