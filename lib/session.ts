48ff1f2c25bde8eec643ebaaa51fee99ef5992ce
jirayu <jirayu004za@gmail.com>
2023-10-12 00:06:04 +0700

    complete with JWT

--- lib/session.ts
+++ lib/session.ts
@@ -2,9 +2,12 @@ import { getServerSession } from "next-auth/next";
 import { NextAuthOptions, User } from "next-auth";
 import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google"
+
+import { SessionInterface, UserProfile } from "@/common.types";
+import { getUser, createUser } from "./actions";
+
 import jsonwebtoken from 'jsonwebtoken';
 import { JWT } from "next-auth/jwt";
-import { SessionInterface } from "@/common.types";
 
 export const authOptions: NextAuthOptions = {
         providers: [
@@ -13,27 +16,59 @@ export const authOptions: NextAuthOptions = {
                 clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
     ],
-    //jwt: {
-    //    encode: ({ secret, token }) => {
-    //        
-    //    },
-    //    decode: async ({ secret, token }) => { 
-    //    }
-    //},
+    jwt: {
+        encode: ({ secret, token }) => {
+            const encodedToken = jsonwebtoken.sign({
+                ...token,
+                iss: 'grafbase',
+                exp: Math.floor(Date.now() / 1000) + 60 * 60
+            }, secret)
+
+            return encodedToken;
+        },
+        decode: async ({ secret, token }) => { 
+            const decodedToken = jsonwebtoken.verify(token!, secret) as JWT;
+            
+            return decodedToken;
+        }
+    },
     theme: {
         colorScheme: 'light',
         logo: '/logo.png',
     },
     callbacks: {
         async session({ session }) {
-            return session;
+            const email = session?.user?.email as string;
+
+            try {
+                const data = await getUser(email) as { user?: UserProfile };
+                const newSession = {
+                    ...session,
+                    user: {
+                        ...session.user,
+                        ...data?.user
+                    }
+                }
+                return newSession;
+            } catch (error) {
+                console.log('Error retrieving user data' + error);
+                return session;
+            }
+            
         },
         async signIn({ user } : { user: AdapterUser | User }) {
             try {
                 // get the user if they exist
-
+                const userExists = await getUser(user?.email as string) as { User?: UserProfile }
                 // if they don't exist, create them
-
+                if(!userExists.User){
+                    // await createUser
+                    await createUser(
+                        user.name as string,
+                        user.email as string,
+                        user.image as string
+                        );
+                }
                 // return the user (true)
                 return true;
             } catch (error: any) {
@@ -48,3 +83,7 @@ export async function getCurrentUser() {
     const session = await getServerSession(authOptions) as SessionInterface;
     return session;
 }
+
+// return a Google user
+// name, email, avatarUrl ->
+// projects, description, githubUrl, linkedInUrl....
